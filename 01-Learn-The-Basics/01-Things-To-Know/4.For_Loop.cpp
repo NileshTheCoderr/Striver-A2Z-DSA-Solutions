e39a4aff664fa1fde5a6a16881cae74dc423dabb
NileshTheCoderr <nilesh.gupta77289@gmail.com>
2026-06-10 21:09:08 +0530

    Rename and organize solution files

---
+++ 01-Learn-The-Basics/01-Things-To-Know/4.For_Loop.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int low,high,sum=0;
+    cout << "enter the value of low and high" << "\n";
+    cin >> low ;
+    cin >> high;
+    
+    for(int i=low;i<=high;i++){
+     sum=sum+i;
+    }
+    cout << sum;
+return 0;
+}
