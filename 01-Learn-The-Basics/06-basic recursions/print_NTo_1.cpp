cf36a3d0ce7cd47fbd7b562cb9d9548de7600435
NileshTheCoderr <nilesh.gupta77289@gmail.com>
2026-06-13 23:35:09 +0530

    Rename and organize solution files

---
+++ 01-Learn-The-Basics/06-basic recursions/print_NTo_1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int n){
+if(n<1){
+    return ;
+}
+cout << n << " ";
+f(n-1);
+}
+
+int main(){
+  int n;
+  cin >> n;
+  f(n) ;
+  return 0;
+}
+
