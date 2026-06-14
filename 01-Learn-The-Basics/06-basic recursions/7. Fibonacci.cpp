51d36bc63ac18b8822d07b27dfb8252e2515f111
NileshTheCoderr <nilesh.gupta77289@gmail.com>
2026-06-14 18:56:09 +0530

    Add and organize solution files

---
+++ 01-Learn-The-Basics/06-basic recursions/7. Fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n){
+if(n<=1){
+    return n;
+}
+int last=f(n-1);
+int slast=f(n-2);
+return last+slast;
+}
+
+int main(){
+    int n;
+    cin >>  n;
+    int fibonacciNum=f(n);
+    cout << fibonacciNum;
+}
