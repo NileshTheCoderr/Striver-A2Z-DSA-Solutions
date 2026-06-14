415d1301b65632f07c7e104904f6d50138e81809
NileshTheCoderr <nilesh.gupta77289@gmail.com>
2026-06-14 10:47:53 +0530

    Add and organize solution files

---
+++ 01-Learn-The-Basics/06-basic recursions/5. ReversingArray.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int l,int r,int arr[]){
+    if(l>=r){
+        return;
+    }
+swap(arr[l],arr[r]);
+f(l+1,r-1,arr);
+}
+
+
+int main(){
+    int l,r,n=5;
+int arr[n]={1,2,3,4,5};
+f(0,n-1,arr);
+for(int i=0;i<n;i++){
+    cout << arr[i];
+}
+return 0;
+}
